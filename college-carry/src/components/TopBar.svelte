5edef906c6b7246b07dc627416e1dbd04aa78863
Dunn, Simeon <simedunn01@gmail.com>
2024-01-01 01:19:50 -0500

    removed svelte ... for now

--- college-carry/src/components/TopBar.svelte
+++
@@ -1,9 +0,0 @@
-<div class="body-overlay"></div>
-<div class="top-bar">
-  <div class="container">
-      <div class="icons-contact">
-          <a href="tel:(260)804-3503"><i class="fa-solid fa-phone"></i><span>260-804-3503</span></a>
-          <a href="mailto:collegecarry@outlook.com?subject=Customer Contact"><i class="fa-solid fa-envelope"></i><span>collegecarry@outlook.com</span></a>
-      </div>
-  </div>
-</div>
