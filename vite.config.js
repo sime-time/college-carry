ae8d0d556770287fd09be56d4ad0939a1bdc2e83
Dunn, Simeon <simedunn01@gmail.com>
2024-03-08 14:46:54 -0500

    restructure files

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: './',
   build: {
+    outDir: "./docs",
     rollupOptions: {
       input: {
         index: 'index.html',
